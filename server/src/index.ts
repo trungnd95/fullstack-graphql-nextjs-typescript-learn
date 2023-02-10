b987a2f4c4e7673b788afad4de685857b338a66a
Trung-Ng <trungnd233@viettel.com.vn>
2023-02-10 15:11:14 +0700

    server: init app running and connect to database

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,30 @@
-console.log("Helloworld!");
+require("dotenv").config();
+import express from 'express';
+import 'reflect-metadata';
+import { DataSource } from "typeorm";
+
+const main = async () => {
+  const appDataSource = new DataSource({
+    type: "postgres",
+    host: "localhost",
+    port: 5432,
+    username: process.env.DB_USERNAME_DEV,
+    password: process.env.DB_PASSWORD_DEV,
+    database: "reddit",
+    synchronize: true,
+    logging: true,
+    entities: [],
+    subscribers: [],
+    migrations: [],
+  })
+  appDataSource
+    .initialize()
+      .then(() => { console.log("Connected to database")})
+      .catch((err) => { console.log("Failed to connect to database ", err)});
+  
+  const app = express();
+  const PORT = process.env.PORT || 4000;
+  app.listen(PORT, () => { console.log("Server is running!")});
+}
+
+main().catch((err) => { console.log(err) });
