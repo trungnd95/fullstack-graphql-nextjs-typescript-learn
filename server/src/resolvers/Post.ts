a3b14b815d8c092e2785139224b682160c7a3a98
Trung-Ng <trungnd233@viettel.com.vn>
2023-02-15 11:07:35 +0700

    server.create-resolvers.PostResolver: add post update,delete mutation

--- server/src/resolvers/Post.ts
+++ server/src/resolvers/Post.ts
@@ -6,7 +6,7 @@ import { PostMutationResponse } from './../types/PostMutationResponse';
 @Resolver()
 export class PostResolver {
   @Mutation(() => PostMutationResponse)
-  async createPost(
+  async create(
     @Arg('createPostInput') createPostInput: PostCreateInput,
   ): Promise<PostMutationResponse> {
     try {
@@ -38,5 +38,64 @@ export class PostResolver {
   @Query(() => Post, { nullable: true })
   async post(@Arg('id', () => ID) id: number): Promise<Post | null> {
     return await Post.findOneBy({ id });
+  }
+
+  @Mutation(() => PostMutationResponse)
+  async update(
+    @Arg('id', () => ID) id: number,
+    @Arg('postUpdateInput') postUpdateInput: PostCreateInput,
+  ): Promise<PostMutationResponse> {
+    try {
+      const existPost = await Post.findOneBy({ id });
+      if (!existPost)
+        return {
+          code: 400,
+          success: false,
+          message: 'Post not found',
+        };
+
+      existPost.title = postUpdateInput.title;
+      existPost.text = postUpdateInput.text;
+      await existPost.save();
+      return {
+        code: 201,
+        success: true,
+        message: 'Post is updated successfully',
+        post: existPost,
+      };
+    } catch (error) {
+      console.log('Error when updating post', error);
+      return {
+        code: 500,
+        success: false,
+        message: `Error when updating post. ${error.message}`,
+      };
+    }
+  }
+
+  @Mutation(() => PostMutationResponse)
+  async delete(@Arg('id', () => ID) id: number): Promise<PostMutationResponse> {
+    try {
+      const existPost = await Post.findOneBy({ id });
+      if (!existPost)
+        return {
+          code: 400,
+          success: false,
+          message: 'Post not found',
+        };
+      await Post.delete({ id });
+      return {
+        code: 201,
+        success: true,
+        message: 'Post is deleted successfully',
+      };
+    } catch (error) {
+      console.log('Error when updating post', error);
+      return {
+        code: 500,
+        success: false,
+        message: `Error when updating post. ${error.message}`,
+      };
+    }
   }
 }
