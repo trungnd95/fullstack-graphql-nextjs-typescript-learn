a5a14f7290847b234a320f02e105d1c42ddb83f3
Trung-Ng <trungnd233@viettel.com.vn>
2023-02-21 16:22:39 +0700

    client: logout user

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,9 +1,27 @@
-import { useMeQuery } from '@/graphql-client/generated/graphql';
+import {
+  MeDocument,
+  MeQuery,
+  useLogoutMutation,
+  useMeQuery,
+} from '@/graphql-client/generated/graphql';
 import { Box, Flex, Heading, Link } from '@chakra-ui/react';
 import NextLink from 'next/link';
 
 function Navbar() {
   const { data, loading: _queryLoading, error: _err } = useMeQuery();
+  const [logout, _] = useLogoutMutation();
+  const handleLogout = async (e: { preventDefault: () => void }) => {
+    e.preventDefault();
+    await logout({
+      update: (cache, _) => {
+        cache.writeQuery<MeQuery>({
+          query: MeDocument,
+          data: { me: null },
+        });
+      },
+    });
+  };
+
   return (
     <Box bg="tan" p={4}>
       <Flex maxW={800} m="auto" justify="space-between" align="center">
@@ -22,7 +40,7 @@ function Navbar() {
             </>
           ) : (
             <>
-              <Link as={NextLink} href="/logout" passHref>
+              <Link as={NextLink} href="#" passHref onClick={handleLogout}>
                 Logout
               </Link>
             </>
