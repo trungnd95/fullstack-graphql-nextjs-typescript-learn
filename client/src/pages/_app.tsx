8dff9aba778cf8762386c2192e40a05299b26587
Trung-Ng <trungnd233@viettel.com.vn>
2023-02-27 15:35:57 +0700

    client: add update init apollo client for nextjs ssr and try fetch all posts

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,14 +1,17 @@
+import { useApollo } from '@/lib/apolloClient';
 import '@/styles/globals.css';
-import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import { ApolloProvider } from '@apollo/client';
 import { ChakraProvider } from '@chakra-ui/react';
 import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
-  const apolloClient = new ApolloClient({
-    uri: 'http://localhost:4000/graphql',
-    cache: new InMemoryCache(),
-    credentials: 'include',
-  });
+  // const apolloClient = new ApolloClient({
+  //   uri: 'http://localhost:4000/graphql',
+  //   cache: new InMemoryCache(),
+  //   credentials: 'include',
+  //   connectToDevTools: true,
+  // });
+  const apolloClient = useApollo(pageProps);
   return (
     <ApolloProvider client={apolloClient}>
       <ChakraProvider>
--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,12 +1,39 @@
-import Navbar from "@/components/Navbar";
+import Navbar from '@/components/Navbar';
+import { Post, PostDocument, usePostQuery } from '@/graphql-client/generated/graphql';
+import { addApolloState, initializeApollo } from '@/lib/apolloClient';
+import { Flex, Spinner } from '@chakra-ui/react';
 
 export default function Home() {
+  const { data, loading } = usePostQuery();
   return (
     <>
       <Navbar />
-      <div>
-        <h1>Home page</h1>
-      </div>
+      {loading ? (
+        <Flex justify="center" align="center">
+          <Spinner />
+        </Flex>
+      ) : (
+        <div>
+          <ul>
+            {data?.posts.map((post: Post) => (
+              <li key={post.id}>{post.title}</li>
+            ))}
+          </ul>
+        </div>
+      )}
     </>
   );
+}
+
+export async function getStaticProps() {
+  const apolloClient = initializeApollo();
+
+  await apolloClient.query({
+    query: PostDocument,
+  });
+
+  return addApolloState(apolloClient, {
+    props: {},
+    //revalidate: 1,
+  });
 }
